0c7fc19974613f4c2bb8583036b94274e59e9975
GH239048 <163350803+GH239048@users.noreply.github.com>
2024-06-24 13:17:30 -0600

    Add files via upload

---
+++ AD_AccountStatus.ps1
@@ -0,0 +1,24 @@
+﻿cls ; date
+$username = ' '
+
+$User = Get-ADUser -Server " " $username -Properties *
+$User | Select-Object -Property * -ExcludeProperty userSMIMECertificate, userCertificate, msExchRecordedName
+$User | Select-Object -Property `
+    Name, `
+    samAccountName, `
+    EmployeeID, `
+    extensionAttribute5, `
+    extensionAttribute7, `
+    Enabled, `
+    accountExpires, `
+    AccountExpirationDate,
+    LastLogonDate, `
+    @{Name="LastLogonTimestamp";Expression={[datetime]::FromFileTime($_.LastLogonTimestamp)}}, `
+    LockedOut, `
+    @{Name="lockoutTime";Expression={[datetime]::FromFileTime($_.lockoutTime)}}, `
+    BadLogonCount, `
+    LastBadPasswordAttempt, `
+    PasswordExpired, `
+    PasswordLastSet,
+    msDS-KeyCredentialLink,
+    altsecurityidentities
